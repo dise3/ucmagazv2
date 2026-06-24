385c12c5cebb48d13bb1899f717624e0e3dea9dc
Твоё имя <@email.com>
2026-06-24 20:13:38 +0300

    test6

--- client/src/pages/Steam.tsx
+++ client/src/pages/Steam.tsx
@@ -36,7 +36,7 @@ const Steam: React.FC<SteamProps> = ({ onBack }) => {
     } else {
         // Если юзер ввел "Я хочу чтобы на баланс пришло 1000 рублей/10$"
         const base = currency === 'RUB' ? val : val * usdRate;
-        return Math.ceil(base * (1 + steamMarkup));
+        return Math.ceil(base * (1 + steamMarkup) + 0.0485);
     }
   };
 
